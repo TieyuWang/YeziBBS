97f351851e20e8863629232da8519991bd0d984c
wangtieyu <wangtieyu@gwm.cn>
2020-02-28 17:33:40 +0800

    IOC

--- app/src/main/java/com/yezi/yezibbs/MainActivity.java
+++ app/src/main/java/com/yezi/yezibbs/MainActivity.java
@@ -2,13 +2,32 @@ package com.yezi.yezibbs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.yezi.baselibrary.ViewID;
+import com.yezi.baselibrary.ViewUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+    @ViewID(R.id.hello_text);
+    private TextView mTextView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewUtils.inject(this);
     }
+
+
 }
---
+++ baselibrary/src/main/java/com/yezi/baselibrary/ViewFinder.java
@@ -0,0 +1,33 @@
+package com.yezi.baselibrary;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * @author : GW00175635
+ * @date : 2020/2/28 17:12
+ * desc   :
+ * version: 1.0
+ */
+public class ViewFinder {
+    private Activity activity;
+    private View view;
+    public ViewFinder(Activity activity) {
+        this.activity = activity;
+    }
+
+    public ViewFinder(View view) {
+        this.view = view;
+    }
+
+    public ViewFinder(Fragment fragment) {
+        this.activity = fragment.getActivity();
+    }
+
+    public View findViewById(int viewId){
+        return activity != null ? activity.findViewById(viewId) : view.findViewById(viewId);
+    }
+}
